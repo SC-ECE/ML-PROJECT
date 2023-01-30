0b895057ff520eb33ca65ed1b3605fb7e58b0861
Arghadeep <78810009+arghadeeppal433@users.noreply.github.com>
2023-01-30 19:38:03 +0530

    Add files via upload

--- discord music bot/jingles-main/botconfig.js
+++ discord music bot/jingles-main/botconfig.js
@@ -1,22 +1,24 @@
 module.exports = {
-  Admins: ["692948554188193793"], // Admins of the bot, I don't know what this do. -Darren.
+  /*The blank values are for the user to enter to host the discord bot 
+  (they are not filled because they breach privacy)*/
+  Admins: [""], // Admins of the bot
   ExpressServer: false, // If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ";", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/nZKH2RTw2Y", // Support Server Link
-  Token: process.env.Token || "ODEwODU3Nzc1NDIwNjcwMDEz.Gp_z29.vWxTX3Dseitf9FgFXAovHQVRzTuGpRb8zMQjGg", // Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "810857775420670013", // Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "xoJvJsLXirtFAqCvvklcTtBWs5QEJbsn", // Discord Client Secret
+  SupportServer: "", // Support Server Link
+  Token: process.env.Token || "", // Discord Bot Token 
+  ClientID: process.env.Discord_ClientID || "", // Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
   ServerDeafen: true, // If you want bot to stay deafened
   DefaultVolume: 100, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
   CallbackURL: "/api/callback", // Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
   "24/7": true, // Make the bot stays in VC 24/7 (when you reboot the bot will **not** automatically rejoin.)
-  CookieSecret: "Pikachu is chutiya", // A cookie for you, cookie for me. make sure you change this value!
+  CookieSecret: "just a thing", // A cookie for you, cookie for me. make sure you change this value!
   IconURL:
-    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", // URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+    "", // URL of all embed author icons 
   EmbedColor: "RANDOM", // Color of most embeds | Custom Hex value are supported. I.e: "#36393F"
-  Permissions: 277062469696, // Bot Inviting Permissions
+  Permissions: 0, // Bot Inviting Permissions
   Website: process.env.Website || "0.0.0.0", // Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
   // If you get invalid oauth, make sure on the discord developer page you set the oauth url to something like: https://example.com/api/callback.
 
@@ -30,10 +32,10 @@ module.exports = {
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "jp-lava.islantay.tk", //- The host name or IP of the lavalink server.
-    port: 443, // The port that lavalink is listening to. This must be a number!
-    pass: "AmeliaWatsonisTheBest**!", //- The password of the lavalink server.
-    secure: true, // Set this to true if the lavalink uses SSL. if not set it to false.
+    host: "", //- The host name or IP of the lavalink server.
+    port: 0, // The port that lavalink is listening to. This must be a number!
+    pass: "", //- The password of the lavalink server.
+    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
     retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
     retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
   },
